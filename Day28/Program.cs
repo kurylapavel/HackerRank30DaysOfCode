83a6e0ec2221565d0d412a7b88326a5cbfde55b4
Pavel <48916290+kurylapavel@users.noreply.github.com>
2021-01-03 13:50:34 +0100

    Days 27-29

---
+++ Day28/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+            List<string> data = new List<string>();
+            for (int NItr = 0; NItr < N; NItr++)
+            {
+                string[] firstNameEmailID = Console.ReadLine().Split(' ');
+
+                data.Add($"{firstNameEmailID[0]} {firstNameEmailID[1]}");
+
+            }
+            data.Sort();
+            foreach (var item in data)
+            {
+                if (item.Contains("@gmail.com"))
+                {
+                    string[] toWrite = item.Split(' ');
+                    Console.WriteLine(toWrite[0]);
+                }
+            }
+        }
+    }
+}
---
+++ Day29/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Day29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = Convert.ToInt32(Console.ReadLine());
+
+            for (int tItr = 0; tItr < t; tItr++)
+            {
+                string[] nk = Console.ReadLine().Split(' ');
+
+                int n = Convert.ToInt32(nk[0]);
+
+                int k = Convert.ToInt32(nk[1]);
+
+                int max = 0;
+                for (int i = 1; i < n; i++)
+                {
+
+                    for (int j = i + 1; j <= n; j++)
+                    {
+                        int x = i & j;
+                        if (x < k && x > max)
+                        {
+                            max = x;
+                        }
+                    }
+
+                }
+
+                Console.WriteLine(max);
+
+            }
+
+
+        }
+    }
+}
